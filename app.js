fe24f78d7f0bd8be443103df25cd450967c93ad7
baba-alaya <babaalayai@gmail.com>
2023-04-27 21:49:08 +0100

    updating

--- app.js
+++ app.js
@@ -11,53 +11,41 @@ const profile = document.getElementById("socials")
 next.addEventListener('click' , function () {
     if(sideBar.classList.contains("translate")) {
         sideBar.classList.remove("translate")
-        console.log(sideBar.classList)
     } else {
         sideBar.classList.add("translate")
-        console.log(sideBar.classList)
     }
 })
 
 prev.addEventListener('click' , function () {
     if(sideBar.classList.contains("translate")) {
         sideBar.classList.remove("translate")
-        console.log(sideBar.classList)
     }
 })
 
 avatar.addEventListener('click' , function () {
-    console.log(profile)
     if(profile.classList.contains("socials_translate")) {
         profile.classList.remove("socials_translate")
-        console.log(profile.classList)
     } else {
         profile.classList.add("socials_translate")
-        console.log(profile.classList)
     }
-    
 })
 profile.addEventListener('drag', function() {
     if(profile.classList.contains("socials_translate")) {
         profile.classList.remove("socials_translate")
-        console.log(profile.classList)
     }
-    console.log("profile")
 })
 
 user.addEventListener('click' , function(){
-    console.log('bake')
     if(user.classList.contains("active")) {
         user.classList.remove("active")
     } else {
         user.classList.add("active")
         creator.classList.remove("active")
         switch_value.textContent = 'Top Users'
-        
     }
 })
 
 creator.addEventListener('click' , function(){
-    console.log('bake')
     if(creator.classList.contains("active")) {
         creator.classList.remove("active")
     } else {
