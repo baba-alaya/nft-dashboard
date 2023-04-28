b25ddef27c7e704559b7e3d08de7976906fd53b7
baba-alaya <babaalayai@gmail.com>
2023-04-28 16:02:11 +0100

    drag

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const avatar = document.getElementById('avatar');
 const sideBar = document.getElementById('pnft')
 const profile = document.getElementById("socials")
 
-next.addEventListener('click' , function () {
+next.addEventListener('click', function () {
     // if(sideBar.classList.contains("translate")) {
     //     sideBar.classList.remove("translate")
     // } else {
@@ -17,28 +17,33 @@ next.addEventListener('click' , function () {
     sideBar.classList.toggle('translate')
 })
 
-prev.addEventListener('click' , function () {
-    if(sideBar.classList.contains("translate")) {
+prev.addEventListener('click', function () {
+    if (sideBar.classList.contains("translate")) {
         sideBar.classList.remove("translate")
     }
 })
 
-avatar.addEventListener('click' , function () {
-    if(profile.classList.contains("socials_translate")) {
+avatar.addEventListener('click', function () {
+    if (profile.classList.contains("socials_translate")) {
         profile.classList.remove("socials_translate")
     } else {
         profile.classList.add("socials_translate")
     }
 })
 
-profile.addEventListener('drag', function() {
-    if(profile.classList.contains("socials_translate")) {
+profile.addEventListener('drag', closeProfile);
+document.querySelector(".creators").addEventListener('drag', closeProfile);
+document.querySelector(".analyse").addEventListener('drag', closeProfile);
+
+function closeProfile() {
+    if (profile.classList.contains("socials_translate")) {
         profile.classList.remove("socials_translate")
     }
-})
+    console.log('drag')
+}
 
-user.addEventListener('click' , function(){
-    if(user.classList.contains("active")) {
+user.addEventListener('click', function () {
+    if (user.classList.contains("active")) {
         user.classList.add("active")
     } else {
         user.classList.add("active")
@@ -47,8 +52,8 @@ user.addEventListener('click' , function(){
     }
 })
 
-creator.addEventListener('click' , function(){
-    if(creator.classList.contains("active")) {
+creator.addEventListener('click', function () {
+    if (creator.classList.contains("active")) {
         creator.classList.add("active")
     } else {
         creator.classList.add("active")
@@ -73,7 +78,7 @@ creator.addEventListener('click' , function(){
 
 
 
-window.addEventListener('load',function(){
-    
+window.addEventListener('load', function () {
+
     user.classList.add("active")
 })
